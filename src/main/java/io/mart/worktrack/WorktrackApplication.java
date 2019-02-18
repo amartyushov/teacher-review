c71716b0836a10d5f531be89f4f2144bcd0ec58e
Aleksandr Martiushov <aleksandr.martiushov@signavio.com>
2019-02-18 08:25:12 +0100

    Gradle shape of the project

---
+++ src/main/java/io/mart/worktrack/WorktrackApplication.java
@@ -0,0 +1,13 @@
+package io.mart.worktrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktrackApplication.class, args);
+	}
+
+}
