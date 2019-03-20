2507dee0607ae2d78946d8e95f68e2914820319f
Aleksandr Martiushov <aleksandr.martiushov@signavio.com>
2019-03-20 15:08:01 +0100

    skeleton for multimodulness

---
+++ core/src/test/java/io/mart/worktrack/WorktrackApplicationTests.java
@@ -0,0 +1,19 @@
+package io.mart.worktrack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorktrackApplicationTests {
+
+	@Test
+	public void assertContextLoads() {
+		assertThat(true).isTrue();
+	}
+
+}
