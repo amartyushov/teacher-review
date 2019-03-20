393cdc70c98218312be6fa40a36c7309e72f2794
Aleksandr Martiushov <aleksandr.martiushov@signavio.com>
2019-03-20 15:21:34 +0100

    renaming of applications

--- analytics/src/main/java/io/mart/AnalyticApplication.java
+++ analytics/src/main/java/io/mart/AnalyticApplication.java
@@ -1,13 +1,13 @@
-package io.mart.worktrack;
+package io.mart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorktrackApplication {
+public class AnalyticApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorktrackApplication.class, args);
+		SpringApplication.run(AnalyticApplication.class, args);
 	}
 
 }
